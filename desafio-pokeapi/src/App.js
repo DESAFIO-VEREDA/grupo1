2f19598f22759b883a0ad373162704225c5f7e4e
renantavaress <renan_velox@hotmail.com>
2022-02-23 09:52:22 -0300

    commit teste

--- desafio-pokeapi/src/App.js
+++ desafio-pokeapi/src/App.js
@@ -1,8 +1,13 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 
 export default function App() {
 const [pokemons, setPokemons] = useState([])
 
+useEffect(() => {
+  fetch('https://pokeapi.co/api/v2/pokemon')
+    .then(response => response.json())
+    .then(res => console.log(res))
+})
 
   return (
     <>
